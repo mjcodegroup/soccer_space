39fc181f0686172e9ec3a82a61ff2ba74325d72a
lucien3941 <eclesiastelucien@gmail.com>
2022-05-26 15:34:07 -0300

    setting app name and icon

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,15 @@ import 'package:footinfo_app/containers/popular_clubs.dart';
 import 'package:footinfo_app/core/model/card_item.dart';
 import 'package:footinfo_app/sidebar/menu_lateral.dart';
 
+///
+import 'package:flutter_launcher_icons/android.dart';
+import 'package:flutter_launcher_icons/constants.dart';
+import 'package:flutter_launcher_icons/custom_exceptions.dart';
+import 'package:flutter_launcher_icons/ios.dart';
+import 'package:flutter_launcher_icons/main.dart';
+import 'package:flutter_launcher_icons/utils.dart';
+import 'package:flutter_launcher_icons/xml_templates.dart';
+
 void main() {
   runApp(MyApp());
 }
